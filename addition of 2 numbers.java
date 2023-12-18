31d5b525aae6a2ba9de0cefecf97bbe1cb7c19da
Valli-170699 <121665255+Valli-170699@users.noreply.github.com>
2023-12-18 20:49:10 +0530

    Create addition of 2 numbers.java

---
+++ addition of 2 numbers.java
@@ -0,0 +1,15 @@
+class Addition{
+public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+int a,b;
+System.out.println("Enter a value");
+a=sc.nextInt();
+  System.out.println("Enter b value");
+b=sc.nextInt();
+
+  c=a+b;
+  System.out.println("the addition of " +a+ "and" +b+ "=" +c);
+
+}
+}
+
